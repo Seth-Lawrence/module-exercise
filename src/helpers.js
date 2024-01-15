e63cc3f65be9f6b06310ca5d0047b6b3ed83564d
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-15 18:31:54 -0500

    completed helpers file

---
+++ src/helpers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function choice(items) {
+  const idx = Math.floor(Math.random()*items.length);
+
+  return items[idx];
+}
+
+function remove(items,item) {
+  const idx = items.indexOf(item);
+
+  if (idx > -1) {
+    return items.splice(idx,1);
+  }
+
+  return undefined;
+}
+
+export {choice, remove};
